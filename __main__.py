27f5c4085c35db7797b0092a7511275411eb1a96
dan <dsandakov88@gmail.com>
2022-05-22 17:20:55 +0300

    0.4 vers

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 import tinkoff.invest as tinvest
 from pandas import DataFrame
+from datetime import datetime, timedelta
 
 token = 't.og8jYwK7ryJy3Ns1EGYcPvOBOFpONQbnNdlOL75ZgJMtXwFWqZXAqD5YEuXJPLKMgF6wAG8ycLhBlClXZg-Ncg'
 
@@ -18,7 +19,26 @@ def get_figi():
     except (TypeError, IndexError):
         print('Тикера не существует, или он был введен неправильно.')
 
+def get_candle():
+    candles = client.market_data.get_candles(
+        figi=get_figi(),
+        from_=datetime.utcnow()-timedelta(days=5),
+        to=datetime.utcnow(),
+        interval=tinvest.CandleInterval.CANDLE_INTERVAL_HOUR
+    )
+    df = DataFrame([{
+        'time': c.time,
+        'volume': c.volume,
+        'open': money(c.open),
+        'close': money(c.close),
+        'high': money(c.high),
+        'low': money(c.low),
+    } for c in candles.candles])
+    return df
 
+def money(value):
+    return value.units + value.nano / 1e9
 
 if __name__ == "__main__":
     with tinvest.Client(token) as client:
+        print(get_candle())
