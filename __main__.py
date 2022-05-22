c6517ffdeaaf3095a187aa03a41b4022058af5a7
dan <dsandakov88@gmail.com>
2022-05-22 19:34:06 +0300

    добавлен график 0.5vers

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 import tinkoff.invest as tinvest
 from pandas import DataFrame
 from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import pandas as pd
 
 token = 't.og8jYwK7ryJy3Ns1EGYcPvOBOFpONQbnNdlOL75ZgJMtXwFWqZXAqD5YEuXJPLKMgF6wAG8ycLhBlClXZg-Ncg'
 
@@ -8,24 +10,25 @@ def get_ticker():
     ticker = input()
     return ticker
 
-def get_figi():
+def get_info(ticker):
     try:
         stocks = DataFrame(
-            client.instruments.shares(instrument_status=tinvest.InstrumentStatus.INSTRUMENT_STATUS_BASE).instruments,
-            columns=['figi', 'ticker']
+            client.instruments.shares(instrument_status=tinvest.InstrumentStatus.INSTRUMENT_STATUS_ALL).instruments,
+            columns=['name', 'figi', 'ticker']
         )
-        figi = stocks[stocks['ticker'] == get_ticker()]['figi'].iloc[0]
-        return figi
+        comp_info = stocks[stocks['ticker'] == ticker]
+        return comp_info
     except (TypeError, IndexError):
         print('Тикера не существует, или он был введен неправильно.')
 
-def get_candle():
+def get_candle(figi):
     candles = client.market_data.get_candles(
-        figi=get_figi(),
-        from_=datetime.utcnow()-timedelta(days=5),
+        figi=figi,
+        from_=datetime.utcnow()-timedelta(days=6),
         to=datetime.utcnow(),
         interval=tinvest.CandleInterval.CANDLE_INTERVAL_HOUR
     )
+
     df = DataFrame([{
         'time': c.time,
         'volume': c.volume,
@@ -39,6 +42,16 @@ def get_candle():
 def money(value):
     return value.units + value.nano / 1e9
 
+def graph(df, name):
+    plt.title='График компании ' + name
+    df.plot(x='time', y='close')
+    plt.show()
+
+
 if __name__ == "__main__":
     with tinvest.Client(token) as client:
-        print(get_candle())
+        ticker = get_ticker()
+        comp_info = get_info(ticker)
+        df = get_candle(comp_info['figi'].iloc[0])
+        graph(df, comp_info['name'].iloc[0])
+
